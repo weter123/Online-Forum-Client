27db433925c08b333203e8a8d3decdd3d6eeac6d
Ahmad Al Gailani <aalgailani@myseneca.ca>
2022-07-06 14:32:15 -0400

    Created Components for the Client layout

---
+++ src/components/LeftMenu.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftMenu = () => {
+    return <div className='leftmenu'>Left Menu</div>;
+}
+
+export default LeftMenu;
---
+++ src/components/RightMenu.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RightMenu = () => {
+    return <div className='rightmenu'>Right Menu</div>;
+}
+
+export default RightMenu;
