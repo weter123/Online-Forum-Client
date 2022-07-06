36515cd6d02712fd0637dfd6a9d02ffae84bc65b
Ahmad Al Gailani <aalgailani@myseneca.ca>
2022-07-06 15:03:48 -0400

    created useWindowDimension Hook and added it to LeftMenu, RightMenu and SideBar components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-
+import { useWindowDimensions } from "../hooks/useWindowDimensions";
 const SideBar = () => {
+    const {width} = useWindowDimensions();
+    if(width <= 768){
+        return null;
+    }
     return <div className='sidebar'>SideBar</div>;
 }
 
