28c751707aa0e4a3e72a3dd2dccc0373342f787a
Ahmad Al Gailani <aalgailani@myseneca.ca>
2022-07-06 17:35:27 -0400

    Created fake service data for client testing

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import { useWindowDimensions } from "../hooks/useWindowDimensions";
+import Category from "../models/Category"
+import { getCategories } from "../services/DataServices";
 const LeftMenu = () => {
     const {width} = useWindowDimensions();
+    const [categories, setCategories] = useState<JSX.Element>(
+        <div>Left Menu</div>
+    )
+    useEffect(()=>{
+        getCategories()
+        .then((categories:Array<Category>) =>{
+            const cats = categories.map((cat)=> {
+                return <li key={cat.id}>{cat.name}</li>
+            });
+            setCategories(<ul className="category">{cats}</ul>);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    },[]);
+
+
     if(width <= 768){
         return null;
     }
-    return <div className='leftmenu'>Left Menu</div>;
+    return <div className='leftmenu'>{categories}</div>;
 }
 
 export default LeftMenu;
