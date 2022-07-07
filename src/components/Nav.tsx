aa392d155c29aea3bd9f3e81a415db928392f2f2
Ahmad Al Gailani <aalgailani@myseneca.ca>
2022-07-07 19:50:10 -0400

    Updated the Nav Component to show sideBar icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,23 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useWindowDimensions } from "../hooks/useWindowDimensions";
 
 const Nav = () => {
-    return <nav className='navigation'>Nav</nav>;
-}
+    const {width} = useWindowDimensions();
+    const getMobileMenu = () => {
+        if(width <= 768){
+            return(
+                <FontAwesomeIcon icon= {faBars} size ="lg" className= "nav-mobile-menu" />
+            );
+        }
+        return null;
+    }
+    return( 
+    <nav className='navigation'>
+        {getMobileMenu()} <strong>SuperForum</strong>
+        </nav>
+    );
+};
 
 export default Nav;
