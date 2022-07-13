cf530c7afc1ed847e77481ba017ff9c6ce867860
Ahmad Al Gailani <dull.x22@gmail.com>
2022-07-13 17:53:12 -0400

    imported ThreadCard Component and related files

--- src/components/areas/main/Main.tsx
+++ src/components/areas/main/Main.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MainHeader from "./MainHeader";
+import { useParams } from "react-router-dom";
+import ThreadCard from "./ThreadCard";
+import { getThreadsByCategory } from "../../../services/DataServices"
+import Category from "../../../models/Category";
 
 const Main = () => {
-    return <main className='content'> Main</main>;
+    const {categoryId} = useParams();
+    const [category,setCategory] = useState<Category | undefined>();
+    const [threadCards, setThreadCards] = useState<Array<JSX.Element> | null>(null);
+
+    useEffect(() => {
+        console.log("main categoryId", categoryId);
+        if(categoryId){
+            let catNum: number = +categoryId;
+            if(catNum > 0){
+                getThreadsByCategory(categoryId).then((threads) =>{
+                    const cards = threads.map((th) => {
+                        return <ThreadCard key ={`thread-${th.id}`} thread = {th} />
+                    });
+                    if(!category) {
+                        setCategory(threads[0].category);
+                    }
+                    setThreadCards(cards);
+                });
+            }
+        }
+    },[categoryId]);
+    return (
+        <main className="content">
+          <MainHeader category={category} />
+          <div>{threadCards}</div>
+        </main>
+      );
 }
 
 export default Main;
