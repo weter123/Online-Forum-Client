532f339214cd40c8ccc27df96745a2ed541155c0
Ahmad Al Gailani <dull.x22@gmail.com>
2022-07-13 11:16:10 -0400

    Moved Area components from App to Home.

---
+++ src/components/routes/Home.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from "react";
+import Main from '../areas/main/Main'
+import SideBar from '../areas/sidebar/SideBar';
+import Nav from '../areas/Nav';
+import RightMenu from '../areas/RightMenu';
+import LeftMenu from '../areas/LeftMenu';
+
+const Home:FC =() =>  {
+    return(
+        <div className="screen-root-container home-container">
+            <div className="navigation">
+                <Nav />
+            </div>
+            <SideBar />
+            <LeftMenu />
+            <Main />
+            <RightMenu />
+        </div>
+    )
+}
+
+export default Home;
