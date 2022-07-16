d49fb00032e9f8aad46ca8e78fdf16619673669c
Ahmad Al Gailani <dull.x22@gmail.com>
2022-07-15 23:39:29 -0400

    Replaced password change functions with PasswordComparison import

--- src/components/auth/Registeration.tsx
+++ src/components/auth/Registeration.tsx
@@ -1,18 +1,19 @@
 import React, { FC, useReducer, useState} from "react";
 import ReactModal from "react-modal";
-import { isPasswordValid, PasswordTestResult } from "../../common/validators/PasswordValidator";
 import {ModalProps} from '../types/ModalProps';
 import userReducer from "./common/UserReducer";
 import { allowSubmit } from "./common/Helpers";
+import PasswordComparison from "./common/PasswordComparison";
 
     const Registration : FC<ModalProps> = ({ isOpen, onClickToggle}) =>{
         const [isRegisterDisabled, setRegisterDisabled,] = useState(true);
-        const[{ userName,password,email, passwordConfirm,resultMsg}, dispatch] = useReducer(userReducer,{
+        const[{ userName,password,email, passwordConfirm,resultMsg, isSubmitDisabled}, dispatch] = useReducer(userReducer,{
             userName: "devac",
             password: "",
             email: "admin@szhaven.com",
              passwordConfirm: "",
              resultMsg: "",
+             isSubmitDisabled: true,
         });
 
        
@@ -35,28 +36,7 @@ import { allowSubmit } from "./common/Helpers";
                 allowSubmit(dispatch,"", false);
             }
         };
-        const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) =>{
-            dispatch({type: "Password", payload: e.target.value});
-            const PasswordCheck: PasswordTestResult = isPasswordValid(e.target.value);
-            if(!PasswordCheck.isValid){
-                allowSubmit(dispatch,PasswordCheck.message, true);
-                return;
-            }
-            passwordsSame(passwordConfirm,e.target.value);
-        };
-        const onChangePasswordConfirm = (e: React.ChangeEvent<HTMLInputElement>) =>{
-            dispatch({type: "passwordConfirm", payload: e.target.value});
-            passwordsSame(passwordConfirm,e.target.value);
-        };
-        const passwordsSame = (passwordVal: string, passwordConfirmVal: string) =>{
-            if(passwordVal !== passwordConfirmVal){
-                allowSubmit(dispatch,"Passwords do not match", true);
-                return false;
-            }else{
-                allowSubmit(dispatch,"", false);
-                return true;
-            }
-        };
+
         const onClickRegister = (e: React.MouseEvent<HTMLButtonElement,MouseEvent>) =>{
             e.preventDefault();
             onClickToggle(e);
@@ -68,30 +48,31 @@ import { allowSubmit } from "./common/Helpers";
 
         return(
             <ReactModal
-            className="modal-menu"
-            isOpen ={isOpen}
-            onRequestClose ={onClickToggle}
-            shouldCloseOnOverlayClick = {true}>
-                <form>
-                    <div className="req-inputs">
-                        <div>
-                            <label>username</label>
-                            <input type ="text" value ={userName} onChange ={onChangeUserName}/>
+                className="modal-menu"
+                isOpen ={isOpen}
+                onRequestClose ={onClickToggle}
+                shouldCloseOnOverlayClick = {true}
+                >
+                    <form>
+                        <div className="req-inputs">
+                            <div>
+                                <label>username</label>
+                                <input type ="text" value ={userName} onChange ={onChangeUserName}/>
+                            </div>
+                            <div>
+                                <label>email</label>
+                                <input type ="text" value ={email} onChange ={onChangeEmail}/>
+                            </div>
+                            <div>
+                                <PasswordComparison
+                                    dispatch={dispatch}
+                                    password={password}
+                                    passwordConfirm={passwordConfirm}
+                                />
+                            </div>
                         </div>
-                        <div>
-                            <label>email</label>
-                            <input type ="text" value ={email} onChange ={onChangeEmail}/>
-                        </div>
-                        <div>
-                            <label>password</label>
-                            <input type ="text" placeholder="Password" value ={password} onChange ={onChangePassword}/>
-                        </div>
-                        <div>
-                            <label>password confirmation</label>
-                            <input type ="text" placeholder="Password Confirmation" value ={passwordConfirm} onChange ={onChangePasswordConfirm}/>
-                        </div>
-                        <div className="req-buttons">
-                            <div className="req-button-left">
+                        <div className="form-buttons">
+                            <div className="form-btn-left">
                                 <button
                                 style={{marginLeft: ".5em"}}
                                 className="action-btn"
@@ -102,14 +83,13 @@ import { allowSubmit } from "./common/Helpers";
                                 className="cancel-btn"
                                 onClick ={onClickCancel}>Close</button>
                             </div>
-                            <span className="reg-btn-right">
+                            <span className="form-btn-right">
                                 <strong>{resultMsg}</strong>
                             </span>
                         </div>
-                    </div>
-                </form>
-            </ReactModal>
-        )
+                    </form>
+                </ReactModal>
+            )
     }
 
     export default Registration;
